42e7aabca23823f0e26c00ec4fce78c3ebefd9e2
Dimitar I <dimitar.iliev123@gmail.com>
2024-08-15 17:37:15 +0200

    Add example for Layouot view

---
+++ LayoutViewsExample/LayoutViewsExample/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutViewsExample.Controllers
+{
+	public class ProductsController : Controller
+	{
+		[Route("products")]
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[Route("search-products/{ProductID?}")]
+		public IActionResult Search(int? ProductID)
+		{
+			ViewBag.ProductID = ProductID;
+			return View();
+		}
+		[Route("order-products")]
+		public IActionResult Order()
+		{
+			return View();
+		}
+	}
+}
