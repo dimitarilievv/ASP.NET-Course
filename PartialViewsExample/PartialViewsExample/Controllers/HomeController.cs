8b747612dcfcf7f3638d2a2ef51163bda4d92eba
Dimitar I <dimitar.iliev123@gmail.com>
2024-08-16 16:43:20 +0200

    Add Assignment and example for Partial Views section

---
+++ PartialViewsExample/PartialViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialViewsExample.Models;
+
+namespace PartialViewsExample.Controllers
+{
+	public class HomeController : Controller
+	{
+		[Route("/")]
+		public IActionResult Index()
+		{
+			//ViewData["ListTitle"] = "Cities";
+			//ViewData["ListItems"]=new List<string>()
+			//{
+			//    "Paris",
+			//    "New York",
+			//    "London",
+			//    "Rome"
+			//};
+			return View();
+		}
+		[Route("about")]
+		public IActionResult About()
+		{
+			return View();
+		}
+		[Route("programming-languages")]
+		public IActionResult ProgrammingLanguages()
+		{
+			ListModel listModel = new ListModel()
+			{
+				ListTitle = "Programming Languages",
+				ListItems = new List<string>()
+				{
+				"Java",
+				"C",
+				"C#",
+				"C++",
+				"Python"
+				}
+			};
+			return PartialView("_ListPartialView", listModel);
+		}
+	}
+}
